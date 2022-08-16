f04e22d74fe5fbbc47ce28a86295f7fde85d2a0b
Ramon Walther <Ramon.Walther@stud.h-da.de>
2022-08-16 20:41:25 +0200

    Initial project setup & sample

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+from main import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(hi('Tester'), "Hi, Tester")
+
+
+if __name__ == '__main__':
+    unittest.main()
