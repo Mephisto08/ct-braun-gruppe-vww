5233c40164428464466ac0a98880de9ee251837e
Niklas Werner <niklas.a.j.werner@stud.h-da.de>
2022-08-17 21:14:07 +0200

    First Test

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ ips = [
 
 class MyTestCase(unittest.TestCase):
     def test_prim_2_poly_3_7x7(self):
-        testMulTab_3 = MulTab(p, P(ips[e]))
+        testMulTab_3 = MulTab(2, P(ips[e]))
         self.assertEqual(testMulTab_3.values[7][7].value, "10")
 
 
