cafaa169f1a6f1097ae06485d82912a8b391a95e
Eliah Vogel <eliah.vogel@stud.h-da.de>
2022-08-25 13:35:49 +0200

    test for praktikum 5

--- test.py
+++ test.py
@@ -25,7 +25,8 @@ def test_multab_e(test, e):
         row_one_counter = 0
         column_one_counter = 0
         for j in range(1, mt.width):
-            test.assertEqual(mt.values[i][j].value, mt.values[j][i].value)  # Check symmetry of matrix
+            # Check symmetry of matrix
+            test.assertEqual(mt.values[i][j].value, mt.values[j][i].value)
 
             # Check for missing modulo
             field = mt.values[i][j].value
@@ -38,21 +39,38 @@ def test_multab_e(test, e):
                 row_one_counter += 1
             if v_column == '1':
                 column_one_counter += 1
-        test.assertEqual(1, row_one_counter)  # Check count of found one's in row
-        test.assertEqual(1, column_one_counter)  # Check count of found one's in column
+        # Check count of found one's in row
+        test.assertEqual(1, row_one_counter)
+        # Check count of found one's in column
+        test.assertEqual(1, column_one_counter)
 
 
 class TestCase(unittest.TestCase):
 
+    # Aufgabe 1
     def test_multab(self):
         for e in range(2, 9):
             print("Running test_multab with e =", e)
             test_multab_e(self, e)
 
+    # Aufgabe 2
     def test_eea(self):
         for e in range(2, 9):
             print("Running test_eea with e =", e)
             test_eea_e(self, e)
+
+    # Aufgabe 3
+
+    # Aufgabe 4
+
+    # Aufgabe 5
+    def test_reed_muller_code(self):
+        print("Running test_reed_muller_code")
+        reed_muller_code = generate_reed_muller_code(1, 3)
+        self.assertEqual(reed_muller_code[0].value, "11111111")
+        self.assertEqual(reed_muller_code[1].value, "01010101")
+        self.assertEqual(reed_muller_code[2].value, "00110011")
+        self.assertEqual(reed_muller_code[3].value, "00001111")
 
 
 if __name__ == '__main__':
